7a70349350ca8763f4677d3b33701b889b342bbc
Victor Murta <vmurtag16@gmail.com>
2021-01-25 21:57:07 -0300

    Aula 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,50 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json'
+
+
+/*npm run dev */
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  * {
+    box-sizing: border-box; 
+  }
+
+  body{
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+
+    /*new styles*/
+    display: flex;
+    flex-direction: column; 
+    font-family: 'Lato', sans-serif;
+
+    //deixa branco no começo
+    color: ${({theme}) => theme.colors.contrastText};
+
+    html,body{
+      min-height: 100vh;
+    }
+
+    #__next{
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+    }
+
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+
       <ThemeProvider theme={theme}>
+      <GlobalStyle />
+        {/*home */}
+        {/* sobre*/}
         <Component {...pageProps} />
       </ThemeProvider>
     </>
