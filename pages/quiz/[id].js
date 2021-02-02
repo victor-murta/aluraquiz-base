e3ffc91ec317f8e05ebbf13bfc568b90a47213a3
Victor Murta <vmurtag16@gmail.com>
2021-02-01 22:53:07 -0300

    link_e_animações

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz'
 
-export default function QuizDaGaleraPage(){
+export default function QuizDaGaleraPage(props){
     return (
-        <div>
-            Desafio da próxima aula junto com as animações
-        </div>
+        <ThemeProvider theme={dbExternal.theme}>
+            <QuizScreen >
+                externalQuestions={dbExterno.questions}
+                externalBg={dbExerno.bg}
+            </QuizScreen>
+        </ThemeProvider>
     );
+    
 }
+export async function getServerSideProps(context){
+    const [projectName, githubUser] = context.query.id.split('___');
+
+    try{
+        const dbExterno = await fetch(`https://${projectName}.${githubUser}.vercel.app`)
+            .then((respostaDoServer) => {
+                if (respostaDoServer.ok){
+                    return respostaDoServer.json();
+                }
+                throw new Error('Falha em pegar os dados');
+            })
+            .then((respostaConvertidaEObjeto) => {
+                return respostaConvertidaEObjeto;
+            })
+            // .catch((err) => {
+            //     console.log(err);
+            // });
+
+            console.log('', dbExterno);
+
+        return {
+            //will be passed to the page component as props
+            props: {
+                dbExterno, 
+            }, 
+        };
+    } catch(err) {
+        throw new Error(err);
+      }
+    }
+
+
